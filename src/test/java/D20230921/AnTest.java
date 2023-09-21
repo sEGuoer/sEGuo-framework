d20eadd7a5cdcc99a6b635a9e6f553424eba4229
sEGuoer <709074535@qq.com>
2023-09-21 17:23:25 +0800

    介绍前两个Test如何测试

--- src/test/java/D20230921/AnTest.java
+++ src/test/java/D20230921/AnTest.java
@@ -2,6 +2,7 @@ package D20230921;
 
 import D20230921.Config.Configurations;
 import com.sEGuo.pojo.User;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -21,11 +22,13 @@ public class AnTest {
         User user2 = configurations.testConfiguration2();
     }
     @Test
+    @DisplayName("要测试方法是否被调用时容器初始化时就调用，用其他两个test测试")
     void LazyTest(){
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Configurations.class);
         Configurations configurations = (Configurations) context.getBean("Configuration1");
         User user1 = configurations.testConfiguration3();
         User user2 = configurations.testConfiguration3();
+
     }
 
 
