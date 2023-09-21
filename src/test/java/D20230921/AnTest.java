3a92b392d1662303aafd7b358a7fff86f7a39cd0
sEGuoer <709074535@qq.com>
2023-09-21 17:38:10 +0800

    将测试语句改为断言（assertions）机制

--- src/test/java/D20230921/AnTest.java
+++ src/test/java/D20230921/AnTest.java
@@ -2,6 +2,7 @@ package D20230921;
 
 import D20230921.Config.Configurations;
 import com.sEGuo.pojo.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -11,24 +12,21 @@ public class AnTest {
     void singleton(){
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Configurations.class);
         Configurations configurations = (Configurations) context.getBean("Configuration1");
-        configurations.testConfiguration1();
+        Assertions.assertNotNull(configurations.testConfiguration1());
     }
 
     @Test
     void prototype(){
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Configurations.class);
         Configurations configurations = (Configurations) context.getBean("Configuration1");
-        User user1 = configurations.testConfiguration2();
-        User user2 = configurations.testConfiguration2();
+//        Assertions.assertSame(configurations.testConfiguration2(),configurations.testConfiguration2());
     }
     @Test
     @DisplayName("要测试方法是否被调用时容器初始化时就调用，用其他两个test测试")
     void LazyTest(){
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Configurations.class);
         Configurations configurations = (Configurations) context.getBean("Configuration1");
-        User user1 = configurations.testConfiguration3();
-        User user2 = configurations.testConfiguration3();
-
+        Assertions.assertSame(configurations.testConfiguration3(),configurations.testConfiguration3());
     }
 
 
