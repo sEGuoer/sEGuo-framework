ce97d7e179689a5ffe09765f078efac37bcb5fde
sEGuoer <709074535@qq.com>
2023-09-22 10:14:09 +0800

    截取systom.out到流中
    便于操作输出内容，进而可以直接用Assertions判断systom.setOut

--- src/test/java/D20230921/AnTest.java
+++ src/test/java/D20230921/AnTest.java
@@ -1,18 +1,30 @@
 package D20230921;
 
 import D20230921.Config.Configurations;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.sEGuo.pojo.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintStream;
+
 public class AnTest {
     @Test
     void singleton(){
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(out);
+        System.setOut(printStream);
+        Assertions.assertEquals("",out.toString());
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Configurations.class);
         Configurations configurations = (Configurations) context.getBean("Configuration1");
         Assertions.assertNotNull(configurations.testConfiguration1());
+        Assertions.assertEquals("Configurations.testConfiguration1"+System.getProperty("line.separator"),out.toString());
+//        System.getProperty("line.separator") = System.lineSeparator()
     }
 
     @Test
