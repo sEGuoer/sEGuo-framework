4ad640cde104ba0276dde89b9f659dea8f2c9ef0
sEGuoer <709074535@qq.com>
2023-09-26 10:28:39 +0800

    AutowiredConfigQualifierTest(New)

--- src/test/java/PrimaryBeanTest.java
+++ src/test/java/PrimaryBeanTest.java
@@ -3,11 +3,13 @@ import com.sEGuo.autoWired.AutowiredPrimaryBean;
 import com.sEGuo.autoWired.PrimaryBean;
 import com.sEGuo.autoWired.PrimaryConfig;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class PrimaryBeanTest {
     @Test
+    @DisplayName("@QualifierTest在通过@Autowired（其他几个@也可以）注入依赖是，指定拿到哪个bean")
     void primaryBean(){
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrimaryConfig.class);
         AutowiredPrimaryBean autowiredPrimaryBean = (AutowiredPrimaryBean) ac.getBean(AutowiredPrimaryBean.class);
