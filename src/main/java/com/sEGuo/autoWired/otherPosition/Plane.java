00f6d9855525c1d0743a553bda5ab75b9155f0de
sEGuoer <709074535@qq.com>
2023-09-26 10:43:40 +0800

    OtherPositionAutowiredConfigTest（new）

--- src/main/java/com/sEGuo/autoWired/otherPosition/Plane.java
+++ src/main/java/com/sEGuo/autoWired/otherPosition/Plane.java
@@ -3,12 +3,25 @@ package com.sEGuo.autoWired.otherPosition;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class Plane {
+    @Autowired
+    Assistant assistant;
     Boss boss;
+
+
+
+    public Assistant getAssistant() {
+        return assistant;
+    }
+
     Car car;
 
     public Plane(@Autowired
                  Boss boss) {
         this.boss = boss;
+    }
+    public Plane(
+                 Car car) {
+        this.car = car;
     }
 
     public Boss getBoss() {
--- src/test/java/OtherPositionAutowiredTest.java
+++ src/test/java/OtherPositionAutowiredTest.java
@@ -2,11 +2,13 @@
 import com.sEGuo.autoWired.otherPosition.OtherPositionAutowiredConfig;
 import com.sEGuo.autoWired.otherPosition.Plane;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OtherPositionAutowiredTest {
     @Test
+    @DisplayName("不同地方的依赖引入，都可以拿到对应的依赖")
     void ohterPositionAutowired(){
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(OtherPositionAutowiredConfig.class);
         Assertions.assertTrue(ac.containsBean("car"));
@@ -14,6 +16,8 @@ public class OtherPositionAutowiredTest {
         Assertions.assertTrue(ac.containsBean("assistant"));
         Plane p = (Plane)ac.getBean("getPlane");
         Assertions.assertNotNull(p.getBoss());
+        Assertions.assertNotNull(p.getCar());
+        Assertions.assertNotNull(p.getAssistant());
 
     }
 }
