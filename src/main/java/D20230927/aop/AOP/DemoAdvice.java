c739889d1f22d0c288d0e6fced1f679e20d2b92d
sEGuoer <709074535@qq.com>
2023-09-27 10:40:04 +0800

    AOPforSingleMethod

---
+++ src/main/java/D20230927/aop/AOP/DemoAdvice.java
@@ -0,0 +1,19 @@
+package D20230927.aop.AOP;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class DemoAdvice {
+    @Pointcut("execution(void D20230927.aop.dao.imp.UserDaoImp.create())")
+    private void pt() {
+    }
+    @Before("pt()")
+    public void startTimeSetBefore(){
+        long startTime = System.currentTimeMillis();
+        System.out.println("startTime=" + startTime);
+    }
+}
--- src/main/java/D20230927/aop/Config/StartTimeConfig.java
+++ src/main/java/D20230927/aop/Config/StartTimeConfig.java
@@ -2,8 +2,10 @@ package D20230927.aop.Config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy
 @ComponentScan("D20230927.aop")
 public class StartTimeConfig {
 }
