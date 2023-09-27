063a7cf92dad0576040b50f4d2037f8372381a67
sEGuoer <709074535@qq.com>
2023-09-27 11:16:23 +0800

    AOP计算程序运行时间

--- src/main/java/D20230927/aop/AOP/DemoAdvice.java
+++ src/main/java/D20230927/aop/AOP/DemoAdvice.java
@@ -1,29 +1,31 @@
 package D20230927.aop.AOP;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class DemoAdvice {
+    private long startTime;
+    private long endTime;
     @Pointcut("execution(void D20230927.aop.dao.imp.UserDaoImp.*())")
     private void pt() {
     }
     @Before("pt()")
     public void startTimeSetBefore(){
-        long startTime = System.currentTimeMillis();
+        startTime = System.currentTimeMillis();
         System.out.println("startTime=" + startTime);
     }
+
 
     @Pointcut("execution(void D20230927.aop.dao.imp.UserDaoImp.destroy())")
     private void pts() {
     }
     @After("pts()")
     public void endTimeSetAfter(){
-        long endTime = System.currentTimeMillis();
+        endTime = System.currentTimeMillis();
         System.out.println("endTime=" + endTime);
+        System.out.println("elapasedTime=" + (startTime - endTime));
     }
+
 }
