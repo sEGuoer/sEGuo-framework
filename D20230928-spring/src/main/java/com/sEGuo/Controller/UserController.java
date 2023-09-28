d7862f485f6a1d3ea3a1d67680c44262c05997ef
sEGuoer <709074535@qq.com>
2023-09-28 17:35:45 +0800

    user/date-time-format
    @DateTimeFormat格式在注释里面看

--- D20230928-spring/src/main/java/com/sEGuo/Controller/UserController.java
+++ D20230928-spring/src/main/java/com/sEGuo/Controller/UserController.java
@@ -3,11 +3,15 @@ package com.sEGuo.Controller;
 import com.sEGuo.Service.UserService;
 import com.sEGuo.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
 
 @Controller
 
@@ -35,5 +39,10 @@ public class UserController {
     @ResponseBody
     String pojoBodyParam(@RequestBody User user){
         return ("UserController.pojoBodyParam " + user.toString());
+    }
+    @RequestMapping("/User/date-time-format")
+    @ResponseBody
+    String dateTimeFormat(Date date, @DateTimeFormat(pattern = "yyyy-MM-dd") Date dateDay){
+        return ("UserController.dateTimeFormat " + dateDay.toString());
     }
 }
