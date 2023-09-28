d3e7402ed79abe099d931d06118e8f1bfd9a3fdd
sEGuoer <709074535@qq.com>
2023-09-28 16:49:42 +0800

    user/Param-pojo-body

--- D20230928-spring/src/main/java/com/sEGuo/Controller/UserController.java
+++ D20230928-spring/src/main/java/com/sEGuo/Controller/UserController.java
@@ -4,6 +4,7 @@ import com.sEGuo.Service.UserService;
 import com.sEGuo.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -32,8 +33,7 @@ public class UserController {
     }
     @RequestMapping("/User/param-pojo-body")
     @ResponseBody
-    String pojoBodyParam(User user){
-        userService.add();
+    String pojoBodyParam(@RequestBody User user){
         return ("UserController.pojoBodyParam " + user.toString());
     }
 }
