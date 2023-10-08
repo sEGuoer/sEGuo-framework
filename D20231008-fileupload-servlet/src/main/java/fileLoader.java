096f5787df370d8dde7dbfae3f067e85fb5b86bc
sEGuoer <709074535@qq.com>
2023-10-08 15:27:19 +0800

    FileUpload基本配置

---
+++ D20231008-fileupload-servlet/src/main/java/fileLoader.java
@@ -0,0 +1,21 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/fileUpload")
+public class fileLoader extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String text = req.getParameter("text");
+        System.out.println(text);
+    }
+}
--- D20231008-fileupload-servlet/src/main/webapp/index.jsp
+++ D20231008-fileupload-servlet/src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<a href="getFile.html">getFile.html</a>
 </body>
 </html>
