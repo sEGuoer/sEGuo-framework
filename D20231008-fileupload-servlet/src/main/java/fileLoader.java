2974bbff635ebcf468b6d379be6220ea9fe047d6
sEGuoer <709074535@qq.com>
2023-10-08 15:55:47 +0800

    页面返回fileUpload上传的图片

--- D20231008-fileupload-servlet/src/main/java/fileLoader.java
+++ D20231008-fileupload-servlet/src/main/java/fileLoader.java
@@ -1,11 +1,14 @@
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.Part;
+import org.apache.commons.io.IOUtils;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 @WebServlet("/fileUpload")
@@ -18,16 +21,20 @@ public class fileLoader extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html; charset=utf-8");
         String text = req.getParameter("text");
         System.out.println(text);
         Part file = req.getPart("file");
         System.out.println("文件名：" + file.getSubmittedFileName());
         System.out.println("文件类型：" + file.getContentType());
         System.out.println("文件大小：" + file.getSize());
-        //把文件写入到磁盘中的指定目录
+        //1.把文件写入到磁盘中的指定目录
         String url = "C:\\Users\\Administrator\\Desktop\\sEGuo-framework\\D20231008-fileupload-servlet\\src\\main\\webapp\\img\\" + file.getSubmittedFileName();
         file.write(url);
-        resp.getWriter().write("文件上传成功：" + file.getSubmittedFileName());
+        FileInputStream fis = new FileInputStream(url);
+        //2. 获取response字节输出流
+        ServletOutputStream os = resp.getOutputStream();
+        //3. 完成流的copy
+        IOUtils.copy(fis,os);
+        fis.close();
     }
 }
