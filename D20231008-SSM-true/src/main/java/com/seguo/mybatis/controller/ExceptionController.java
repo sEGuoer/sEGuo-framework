399b64ce64cd366d187cc60acf143b2ea428d838
sEGuoer <709074535@qq.com>
2023-10-10 15:03:43 +0800

    @ExceptionHandler处理错误
    处理ArithmeticException若错误不为这个才去当成Exception处理

--- D20231008-SSM-true/src/main/java/com/seguo/mybatis/controller/ExceptionController.java
+++ D20231008-SSM-true/src/main/java/com/seguo/mybatis/controller/ExceptionController.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionController {
+    @ExceptionHandler(ArithmeticException.class)
+    R arithmeticException(ArithmeticException a) {
+        System.out.println(a.getMessage());
+        return R.ok();
+    }
+
     @ExceptionHandler(Exception.class)
-    R excepiton(Exception e){
+    R excepiton(Exception e) {
         System.out.println(e.getMessage());
         return R.ok();
     }
