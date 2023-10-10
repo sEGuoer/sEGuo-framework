e4ca9cffbe258baae97f5f50a638df7a0a1c0e31
sEGuoer <709074535@qq.com>
2023-10-10 15:35:40 +0800

    excludePathPatterns
    implements HandlerInterceptor

--- D20231008-SSM-true/src/main/java/com/seguo/mybatis/config/MyWebConfig.java
+++ D20231008-SSM-true/src/main/java/com/seguo/mybatis/config/MyWebConfig.java
@@ -19,6 +19,6 @@ public class MyWebConfig implements HandlerInterceptor , WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new InterceptorConfig("x")).addPathPatterns("/HelloSpringWebMvc/i");
+        registry.addInterceptor(new InterceptorConfig("x")).addPathPatterns("/HelloSpringWebMvc/*").excludePathPatterns("/HelloSpringWebMvc/j");
     }
 }
