64bb73c255fdc22954bd80a23237a7d2127b2a21
sEGuoer <709074535@qq.com>
2023-10-10 09:21:16 +0800

    CommonResponse -->R

--- D20231008-SSM-true/src/main/java/com/seguo/mybatis/controller/BlogController.java
+++ D20231008-SSM-true/src/main/java/com/seguo/mybatis/controller/BlogController.java
@@ -1,15 +1,11 @@
 package com.seguo.mybatis.controller;
 
-import com.seguo.mybatis.method.CommonResponse;
+import com.seguo.mybatis.method.R;
 import com.seguo.mybatis.po.Blog;
 import com.seguo.mybatis.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.io.IOException;
-import java.util.List;
 
 @Controller
 @RequestMapping("/blog")
@@ -27,22 +23,22 @@ public class BlogController {
 
     @GetMapping
     @ResponseBody
-    CommonResponse index(@RequestParam(defaultValue = "0")int page, @RequestParam(defaultValue = "0")int perpage) {
+    R index(@RequestParam(defaultValue = "0")int page, @RequestParam(defaultValue = "0")int perpage) {
         if (page == 0 && perpage == 0){
-            return CommonResponse.ok(userService.selectAllBlog());
+            return R.ok(userService.selectAllBlog());
         }else {
-            return CommonResponse.ok(userService.selectBlogByPage(page,perpage));
+            return R.ok(userService.selectBlogByPage(page,perpage));
         }
     }
     @PostMapping
     @ResponseBody
-    CommonResponse store(Blog blog) {
-        return CommonResponse.ok(userService.addNewBlog(blog));
+    R store(Blog blog) {
+        return R.ok(userService.addNewBlog(blog));
     }
 
     @DeleteMapping("/{id}")
     @ResponseBody
-    CommonResponse destory(@PathVariable String id) {
-        return CommonResponse.ok(userService.deleteBlogByID(id));
+    R destory(@PathVariable String id) {
+        return R.ok(userService.deleteBlogByID(id));
     }
 }
