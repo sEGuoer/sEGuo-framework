54210877e61bc96d8337fc6eee2b77bb2811f949
sEGuoer <709074535@qq.com>
2023-10-11 15:10:05 +0800

    将自己的项目打成jar包然后在新建的module中引入观察效果

--- MyselvesServlet/src/main/java/com/seguo/Filter/SimplyFilter.java
+++ MyselvesServlet/src/main/java/com/seguo/Filter/SimplyFilter.java
@@ -4,13 +4,15 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
 
 import java.io.IOException;
-
+@WebFilter("/test")
 public class SimplyFilter extends HttpFilter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         System.out.println("SimplyFilter.doFilter");
+        chain.doFilter(request,response);
     }
 }
