05a8c20b025a4ff4ddec376691e4b1e875261dc5
sEGuoer <709074535@qq.com>
2023-10-25 08:47:17 +0800

    new sb module

---
+++ D20231025-sb/src/main/java/com/seguoer/d20231025sb/D20231025SbApplication.java
@@ -0,0 +1,13 @@
+package com.seguoer.d20231025sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D20231025SbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(D20231025SbApplication.class, args);
+    }
+
+}
---
+++ D20231025-sb/src/test/java/com/seguoer/d20231025sb/D20231025SbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seguoer.d20231025sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D20231025SbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
