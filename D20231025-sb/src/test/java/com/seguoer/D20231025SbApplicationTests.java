4e45c388dea22ba0583ade000085ec767f129566
sEGuoer <709074535@qq.com>
2023-10-26 09:00:10 +0800

    ui Test

--- D20231025-sb/src/test/java/com/seguoer/D20231025SbApplicationTests.java
+++ D20231025-sb/src/test/java/com/seguoer/D20231025SbApplicationTests.java
@@ -1,13 +1,38 @@
 package com.seguoer;
 
+import org.hamcrest.core.StringContains;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class D20231025SbApplicationTests {
 
+    @Autowired
+    MockMvc mvc;
+
     @Test
-    void contextLoads() {
+    @DisplayName("重定向之后显示对应页面")
+    void swaggerUI() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/swagger-ui.html"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/swagger-ui/index.html"))
+        ;
+    }
+
+    @Test
+    @DisplayName("直接定位到跳转网址 ")
+    void swaggerUIActual() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/swagger-ui/index.html"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(StringContains.containsString("<div id=\"swagger-ui\">")))
+        ;
     }
 
 }
